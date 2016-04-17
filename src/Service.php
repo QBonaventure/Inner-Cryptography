b21ab9eb0d61dc21136705b6243a50bbf9bb4ca1
QBonaventure <quentin@bonaventure.xyz>
2016-04-17 18:45:07 +0200

    Fixed Service config setting

--- src/Service.php
+++ src/Service.php
@@ -5,24 +5,44 @@ use Zend\Config\Config;
 
 class Service {
 	
-	const METHOD	= 'aes-256-cbc';
-	const HASH_ALGO = 'sha256';
+	/**
+	 * @var Config
+	 */
+	protected $config;
+	
+	protected $method	= 'aes-256-cbc';
+	protected $hashAlgo = 'sha256';
 	protected $key;
 	protected $iv;
 	
 	public function __construct(Config $config) {
+		$this->setConfig($config);
+	}
+	
+	public function getConfig() {
+		return $this->config;
+	}
+	
+	protected function setConfig(Config $config) {
+		$this->config	= $config;
 		$this->key	= $config['key'];
 		$this->iv	= $config['IV'];
+		if($config['hash_algo'])
+			$this->hashAlgo	= $config['hash_algo'];
+		if($config['method'])
+			$this->method	= $config['method'];
+		
+		return $this;
 	}
 	
 	
-	public static function weakEncrypt($message, $key = null, $encode = false, $iv = null) {
+	public function weakEncrypt($message, $key = null, $encode = false, $iv = null) {
 		if(is_null($key))
 			$key	= $this->key;
 		if(is_null($iv))
 			$iv	= $this->iv;
 		
-		$crypt	= @openssl_encrypt($message, self::METHOD, $key, 'OPENSSL_ZERO_PADDING', $iv);
+		$crypt	= @openssl_encrypt($message, $this->method, $key, 'OPENSSL_ZERO_PADDING', $iv);
 		if($encode)
 			return base64_encode($crypt);
 			else
@@ -30,7 +50,7 @@ class Service {
 	}
 	
 	
-	public static function weakDecrypt($message, $key = null, $encoded = false, $iv = null) {
+	public function weakDecrypt($message, $key = null, $encoded = false, $iv = null) {
 		if(is_null($key))
 			$key	= $this->key;
 		if(is_null($iv))
@@ -38,7 +58,7 @@ class Service {
 		
 		if($encoded)
 			$message	= base64_decode($message);
-			return @openssl_decrypt($message, self::METHOD, $key, 'OPENSSL_ZERO_PADDING', $iv);
+			return @openssl_decrypt($message, $this->method, $key, 'OPENSSL_ZERO_PADDING', $iv);
 	}
 	
 	
@@ -49,18 +69,18 @@ class Service {
 		list($encKey, $authKey) = self::splitKeys($key);
 	
 	
-		$ivSize = openssl_cipher_iv_length(self::METHOD);
+		$ivSize = openssl_cipher_iv_length($this->method);
 		$iv = openssl_random_pseudo_bytes($ivSize);
 	
 		$cyphertext = @openssl_encrypt($message,
-				self::METHOD,
+				$this->method,
 				$encKey,
 				'OPENSSL_ZERO_PADDING',
 				$iv);
 	
 		$cyphertext	= $iv . $cyphertext;
 	
-		$mac = hash_hmac(self::HASH_ALGO, $cyphertext, $authKey, true);
+		$mac = hash_hmac($this->hashAlgo, $cyphertext, $authKey, true);
 	
 		if ($encode)
 			return base64_encode($mac).base64_encode($cyphertext);
@@ -69,7 +89,7 @@ class Service {
 	
 	
 	
-	public static function decrypt($message, $key = null, $encoded = false) {
+	public function decrypt($message, $key = null, $encoded = false) {
 		if(is_null($key))
 			$key	= $this->key;
 		
@@ -82,12 +102,12 @@ class Service {
 			$ciphertext = base64_decode(mb_substr($message, $hs));
 		}
 		else {
-			$hs = mb_strlen(hash(self::HASH_ALGO, '', true), '8bit');
+			$hs = mb_strlen(hash($this->hashAlgo, '', true), '8bit');
 			$mac = mb_substr($message, 0, $hs, '8bit');
 			$ciphertext = mb_substr($message, $hs, mb_strlen($message), '8bit');
 		}
 	
-		$calculated = hash_hmac(self::HASH_ALGO,
+		$calculated = hash_hmac($this->hashAlgo,
 				$ciphertext,
 				$authKey,
 				true);
@@ -97,32 +117,32 @@ class Service {
 		}
 	
 	
-		$ivSize = openssl_cipher_iv_length(self::METHOD);
+		$ivSize = openssl_cipher_iv_length($this->method);
 		$iv = mb_substr($ciphertext, 0, $ivSize, '8bit');
 	
 		$plaintext = @openssl_decrypt($ciphertext,
-				self::METHOD,
+				$this->method,
 				$encKey,
 				'OPENSSL_ZERO_PADDING',
 				$iv);
 	
-		return mb_substr($plaintext, openssl_cipher_iv_length(self::METHOD));
+		return mb_substr($plaintext, openssl_cipher_iv_length($this->method));
 		return $plaintext;
 	}
 	
 	
-	protected static function splitKeys($masterKey) {
+	protected function splitKeys($masterKey) {
 		// You really want to implement HKDF here instead!
-		return array(hash_hmac(self::HASH_ALGO, 'ENCRYPTION', $masterKey, true),
-				hash_hmac(self::HASH_ALGO, 'AUTHENTICATION', $masterKey, true));
+		return array(hash_hmac($this->hashAlgo, 'ENCRYPTION', $masterKey, true),
+				hash_hmac($this->hashAlgo, 'AUTHENTICATION', $masterKey, true));
 	}
 	
 	
-	protected static function hashEquals($a, $b) {
+	protected function hashEquals($a, $b) {
 		if (function_exists('hash_equals')) {
 			return hash_equals($a, $b);
 		}
 		$nonce = openssl_random_pseudo_bytes(32);
-		return hash_hmac(self::HASH_ALGO, $a, $nonce) === hash_hmac(self::HASH_ALGO, $b, $nonce);
+		return hash_hmac($this->hashAlgo, $a, $nonce) === hash_hmac($this->hashAlgo, $b, $nonce);
 	}
 }
